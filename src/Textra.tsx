21b7f49f41739e7df595edbebb7b96a7cf45899d
Hosein <Hosein2398@gmail.com>
2023-03-15 23:06:45 +0330

    feat: make code more readable

--- src/Textra.tsx
+++ src/Textra.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-use-before-define
 import React, { useEffect, useRef, useState, useCallback } from 'react'
 import animationStyles from './animationStyles'
-import type { AnimateObject } from './animationStyles'
+import type { AnimateObject, Animations } from './animationStyles'
 
 type TextraProps = {
   data: string[];
-  effect: keyof typeof animationStyles;
+  effect?: Animations;
   duration?: number;
   stopDuration?: number;
   className?: string;
@@ -20,11 +20,10 @@ const Textra = (props: TextraProps) => {
   const textArrIndex = useRef(0)
   const [text, setText] = useState<string>(props.data[textArrIndex.current])
   const previousTime = useRef<number | null>(null)
-  const duration = props.duration || 500
+  const animationDuration = props.duration || 500
   const stopDuration = props.stopDuration || 3000
-  const initialRound = stopDuration + duration
-  const currentRoundTime = useRef(initialRound)
-  const singleRoundTime = stopDuration + 2 * duration
+  const currentRoundStartTime = useRef(0)
+  const singleRoundTime = stopDuration + 2 * animationDuration
   const hasUpdatedText = useRef(false)
   const easeOutQuad = (t: number): number => t * (2 - t)
 
@@ -34,43 +33,49 @@ const Textra = (props: TextraProps) => {
 
   useEffect(() => {
     setText(props.data[textArrIndex.current])
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [textArrIndex.current, props.data])
 
-  const runShowTextAnimation = (elapsed:number) => {
-    const showingTranlateInitial = animationStyles[selectedAnimation][1].translate.value
-    const showingTranlateDiffrence = animationStyles[selectedAnimation][1].translate.value - animationStyles[selectedAnimation][0].translate.value
-    // const showingOpacityInitial = animationStyles[selectedAnimation][1].opacity
-    // const showingOpacityDiffrence = animationStyles[selectedAnimation][1].opacity - (animationStyles[selectedAnimation][1].opacity - animationStyles[selectedAnimation][0].opacity)
-    const showingTiming = easeOutQuad((elapsed - currentRoundTime.current) / duration)
+  const runShowTextAnimation = (elapsed: number) => {
+    const showingTranlateInitial =
+      animationStyles[selectedAnimation][1].translate.value
+    const showingTranlateDiffrence =
+      animationStyles[selectedAnimation][1].translate.value -
+      animationStyles[selectedAnimation][0].translate.value
+    const showingTiming = easeOutQuad(
+      (elapsed - currentRoundStartTime.current) / animationDuration
+    )
     setStyle((s) => ({
       translate: {
         ...s.translate,
-        value:
-          showingTranlateInitial - showingTranlateDiffrence * showingTiming
+        value: showingTranlateInitial - showingTranlateDiffrence * showingTiming
       },
       opacity: showingTiming
     }))
   }
 
-  const runHideTextAnimation = (elapsed:number) => {
-    const hidingTraslateInitial = animationStyles[selectedAnimation][0].translate.value
-    const hidingTranslateDiffrence = animationStyles[selectedAnimation][0].translate.value - animationStyles[selectedAnimation][1].translate.value
+  const runHideTextAnimation = (elapsed: number) => {
+    const hidingTraslateInitial =
+      animationStyles[selectedAnimation][0].translate.value
+    const hidingTranslateDiffrence =
+      animationStyles[selectedAnimation][0].translate.value -
+      animationStyles[selectedAnimation][1].translate.value
     const hidingOpacityInitial = animationStyles[selectedAnimation][0].opacity
-    // const hidingOpacityDiffrence = animationStyles[selectedAnimation][1].opacity - (animationStyles[selectedAnimation][0].opacity - animationStyles[selectedAnimation][1].opacity)
-    const hidingTiming = easeOutQuad((elapsed - (currentRoundTime.current - duration)) / duration)
+    const hidingTiming = easeOutQuad(
+      (elapsed - (currentRoundStartTime.current + singleRoundTime - animationDuration)) /
+        animationDuration
+    )
     setStyle((s) => ({
       translate: {
         ...s.translate,
-        value:
-        hidingTraslateInitial - hidingTranslateDiffrence * hidingTiming
+        value: hidingTraslateInitial - hidingTranslateDiffrence * hidingTiming
       },
       opacity: hidingOpacityInitial - hidingTiming
     }))
   }
 
   const updateTextIndex = () => {
-    if (textArrIndex.current === (props.data.length - 1)) {
+    if (textArrIndex.current === props.data.length - 1) {
       textArrIndex.current = 0
     } else {
       textArrIndex.current = textArrIndex.current + 1
@@ -83,33 +88,33 @@ const Textra = (props: TextraProps) => {
       const elapsed = timestamps - (previousTime.current as number)
 
       if (
-        elapsed > currentRoundTime.current &&
-        elapsed < currentRoundTime.current + duration
+        elapsed > currentRoundStartTime.current &&
+        elapsed < currentRoundStartTime.current + animationDuration
       ) {
         runShowTextAnimation(elapsed)
       }
       if (
-        elapsed > currentRoundTime.current - duration &&
-        elapsed < currentRoundTime.current
+        elapsed > currentRoundStartTime.current + singleRoundTime - animationDuration &&
+        elapsed < currentRoundStartTime.current + singleRoundTime
       ) {
         runHideTextAnimation(elapsed)
       }
 
-      if (elapsed > currentRoundTime.current) {
+      if (elapsed > currentRoundStartTime.current + singleRoundTime) {
         if (!hasUpdatedText.current) {
           updateTextIndex()
           hasUpdatedText.current = true
         }
       }
 
-      if (elapsed > currentRoundTime.current + duration) {
+      if (elapsed > currentRoundStartTime.current + singleRoundTime) {
         hasUpdatedText.current = false
-        currentRoundTime.current += singleRoundTime
+        currentRoundStartTime.current += singleRoundTime
       }
 
       animationRef.current = window.requestAnimationFrame(runAnimation)
     },
-    [selectedAnimation, props.data.length, singleRoundTime, duration]
+    [selectedAnimation, props.data.length, singleRoundTime, animationDuration]
   )
 
   useEffect(() => {
@@ -121,6 +126,8 @@ const Textra = (props: TextraProps) => {
   return (
     <>
       <span
+        aria-live='polite'
+        aria-relevant='text removals'
         style={{
           display: 'inline-block',
           transform: `${style.translate.type}(${style.translate.value}${style.translate.unit})`,
