b504b7e06115fe68060de772d830d1b18645fcf6
hosein2398 <hosein2398@gmail.com>
2023-03-20 01:09:16 +0330

    feat: add onUpdate event test

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -4,6 +4,10 @@ import { render, screen, waitForElementToBeRemoved } from '@testing-library/reac
 import '@testing-library/jest-dom'
 import Textra from '../src/Textra'
 
+const wait = (time) => new Promise((resolve, reject) => setTimeout(() => {
+  resolve(true)
+}, time))
+
 beforeEach(() => {
   jest.setTimeout(15000)
 })
@@ -93,4 +97,29 @@ it('should render properly with duration and stopDuration', async () => {
   await waitForElementToBeRemoved(() => screen.queryByText('one'), { timeout: 3200 })
   await screen.findByText('two', {}, { timeout: 200 })
   await waitForElementToBeRemoved(() => screen.queryByText('two'), { timeout: 62000 })
+})
+
+it('should call onUpdate with correct index', async () => {
+  const spy = jest.fn()
+  render(
+    <div>
+      <Textra
+        onUpdate={spy}
+        effect='rightLeft'
+        duration={100}
+        stopDuration={1000}
+        data={['one', 'two']}
+      />
+    </div>
+  )
+  await wait(1400)
+  console.log('callsss', spy.mock.calls)
+  // animates "two" in
+  expect(spy.mock.calls[0][0]).toBe(1)
+  await wait(1400)
+  console.log('callsss after', spy.mock.calls)
+  // returns back to first item ("one")
+  expect(spy.mock.calls[1][0]).toBe(0)
+  // has been called twice till now
+  expect(spy.mock.calls).toHaveLength(2)
 })
